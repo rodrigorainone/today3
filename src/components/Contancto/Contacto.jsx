3e8266f754f5d62b5f82d90a40449cd765b895eb
Tuky <rodri_cjs22@hotmail.com>
2023-06-14 20:30:37 -0300

    reset form y navigate

--- src/components/Contancto/Contacto.jsx
+++ src/components/Contancto/Contacto.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 const Contacto = () => {
 
      const datosFormulario = React.useRef();
+     let navigate = useNavigate()
      
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -22,8 +23,12 @@ const Contacto = () => {
 		${cliente.email}%0A
 		*Indica Tu Consulta :*%0A
 		${cliente.comentarios}%0A `     
+        
 
         window.open(url);
+        e.target.reset()
+        navigate('/')
+
       };
 
     return (
